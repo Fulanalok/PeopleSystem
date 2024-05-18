4847c9186f47ca2df42bba0ee7bacd4d39ad5a3a
Lucas Vilhena <66475011+Fulanalok@users.noreply.github.com>
2024-05-18 12:39:11 -0300

    Update Dependente.java

--- Pipousystem/src/tech/angelofdiasg/pessoas/Dependente.java
+++ Pipousystem/src/tech/angelofdiasg/pessoas/Dependente.java
@@ -1,16 +1,43 @@
-package tech.angelofdiasg.pessoas;
+package tech.angelofdias.pessoas;
 
+import tech.angelofdias.composicoes.Endereco;
+import tech.angelofdias.composicoes.Telefone;
+import tech.angelofdias.interfaces.Cadastramento;
+
+import java.time.LocalDate;
 import java.util.UUID;
 
-public class Dependente {
+public class Dependente extends Pessoa implements Cadastramento {
     private UUID id;
     private Cliente ClienteParente;
+    private Funcionario FuncionarioParente;
+    private String parentesco;
 
+    public Dependente(String nome, LocalDate dataNascimento, Endereco endereco, Telefone telefone, UUID id, Cliente clienteParente, Funcionario funcionarioParente) {
+        super(nome, dataNascimento, endereco, telefone);
+        this.id = id;
+    }
     public Cliente getClienteParente() {
         return ClienteParente;
     }
-
     public void setClienteParente(Cliente clienteParente) {
         ClienteParente = clienteParente;
     }
+    public Funcionario getFuncionarioParente() {
+        return FuncionarioParente;
+    }
+
+    public void setFuncionarioParente(Funcionario funcionarioParente) {
+        FuncionarioParente = funcionarioParente;
+    }
+    @Override
+    public int obterIdade() {
+        return idade;
+    }
+
+    @Override
+    public void cadastrar() {
+    }
+    public void geradordeId() {
+    }
 }
